d013a48c0e2f0965cfdecc3c28feae5d51ae18e6
TJuve <TJuve92@gmail.com>
2014-05-02 17:01:39 -0700

    Started to add icons to the home page. It isn't working correctly :(

--- OneDrinkAway/gen/com/onedrinkaway/R.java
+++ OneDrinkAway/gen/com/onedrinkaway/R.java
@@ -1088,13 +1088,14 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_help=0x7f05003e;
-        public static final int action_home=0x7f05003f;
+        public static final int action_help=0x7f050045;
+        public static final int action_home=0x7f050046;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int advanced_search=0x7f050042;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
@@ -1108,6 +1109,7 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int favorites=0x7f050041;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1125,6 +1127,10 @@ containing a value of this type.
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
+        public static final int search_by_category=0x7f05003e;
+        public static final int search_by_flavor=0x7f050040;
+        public static final int search_by_ingredient=0x7f05003f;
+        public static final int search_by_name=0x7f05003d;
         public static final int search_close_btn=0x7f050038;
         public static final int search_edit_frame=0x7f050034;
         public static final int search_go_btn=0x7f05003a;
@@ -1141,6 +1147,7 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int try_something_new=0x7f050043;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1249,10 +1256,17 @@ containing a value of this type.
         public static final int action_help=0x7f0a000f;
         public static final int action_home=0x7f0a0010;
         public static final int action_settings=0x7f0a0012;
+        public static final int advanced_search=0x7f0a0019;
         public static final int app_name=0x7f0a000d;
+        public static final int favorites=0x7f0a0018;
         public static final int hello_world=0x7f0a0011;
+        public static final int search_by_category=0x7f0a0015;
+        public static final int search_by_flavor=0x7f0a0017;
+        public static final int search_by_ingredient=0x7f0a0016;
+        public static final int search_by_name=0x7f0a0014;
         public static final int title_activity_home_page=0x7f0a0013;
         public static final int title_activity_one_drink_away=0x7f0a000e;
+        public static final int try_something_new=0x7f0a001a;
     }
     public static final class style {
         /** 
