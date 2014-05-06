d2b6b77fb1dd14481f338421c92a32ee39d35ade
lueyja1 <lueyja1@gmail.com>
2014-05-06 09:46:01 -0700

    Removed old methods and updated to Drink instead of Instance

--- OneDrinkAway/src/com/onedrinkaway/machinelearning/MLModel.java
+++ OneDrinkAway/src/com/onedrinkaway/machinelearning/MLModel.java
@@ -15,19 +15,11 @@ public interface MLModel {
 	/**
 	 * @param trainingSet: List of training examples to create the model
 	 **/ 
-	public void train(List<Instance> trainingSet);
+	public void train(List<Drink> trainingSet);
 	
 	/**
 	 * @param sample: the Drink to predict the rating
 	 * @return the predicted rating (0.0-5.0)
 	 **/
 	public double predictRating(Drink sample);
-
-	/**
-	 * 
-	 * @param curIns: the current instance
-	 * @return: the class to rating mapping
-	 * @throws IOException
-	 */
-	public Map<String, Double> predict(Instance curIns) throws IOException;
 }
