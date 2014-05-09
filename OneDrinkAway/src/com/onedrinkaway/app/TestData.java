94ad36d4d36ec40bc229d978946a7de268ff7954
Andrea <Andrea@d-108-179-185-25.dhcp4.washington.edu>
2014-05-09 14:14:46 -0700

    Added flavors to string.xml

--- OneDrinkAway/src/com/onedrinkaway/app/TestData.java
+++ OneDrinkAway/src/com/onedrinkaway/app/TestData.java
@@ -1,6 +1,9 @@
 package com.onedrinkaway.app;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.onedrinkaway.common.Drink;
 import com.onedrinkaway.common.Drink.Category;
@@ -14,5 +17,24 @@ public class TestData {
 					"3/4 ox Simple syrup", "1 1/2 oz Whiskey"};
 	public static String whiskeySourDescription = "Add all the ingredients to a shaker and fill with ice. Shake, and "
 			+ "strain into a rocks glass filled with fresh ice. Garnish with a cherry and/or lemon wedge if desired.";
+	
+	
+	public static Map<String, Integer> interpretFlavors(Drink drink) {
+		Map<String, Integer> flavors = new TreeMap<String, Integer>();
+		int[] attributes = drink.attributes;
+		flavors.put("Sweet", attributes[0] );
+		flavors.put("Citrusy", attributes[1]);
+		flavors.put("Bitter", attributes[2]);
+		flavors.put("Herbal", attributes[3]);
+		flavors.put("Minty", attributes[4]);
+		flavors.put("Fruity", attributes[5]);
+		flavors.put("Sour", attributes[6]);
+		flavors.put("Boozy", attributes[7]);
+		flavors.put("Spicy", attributes[8]);
+		flavors.put("Salty", attributes[9]);
+		flavors.put("Creamy", attributes[10]);
+		
+		return flavors;
+	}
 	
 }
