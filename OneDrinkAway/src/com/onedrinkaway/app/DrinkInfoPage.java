8d3bd04bfb6ff2825fa6fba3ee9d2e506efbd561
Andrea <Andrea@d-108-179-185-25.dhcp4.washington.edu>
2014-05-09 09:26:44 -0700

    Unformatted DrinkInfo page

--- OneDrinkAway/src/com/onedrinkaway/app/DrinkInfoPage.java
+++ OneDrinkAway/src/com/onedrinkaway/app/DrinkInfoPage.java
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.onedrinkaway.R;
+import com.onedrinkaway.common.Drink;
 
 public class DrinkInfoPage extends OneDrinkAwayActivity {
 
@@ -14,14 +17,40 @@ public class DrinkInfoPage extends OneDrinkAwayActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_drink_info_page);
+		helpID = R.string.okay;
+		Drink whiskeySour = TestData.whiskeySour;
+		
+		setTitle(whiskeySour.name);
+		fillIngredients();
+		setGlassPicture();
+		
+		
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
-
-
+	
+	/**
+	 * Fills the drink_info_ingredients TextView with given drink ingredients list
+	 */
+	private void fillIngredients() {
+		TextView ingredientsTextView = (TextView) findViewById(R.id.drink_info_ingredients);
+		
+		String[] ingredients = TestData.whiskeySourIngredients;
+		ingredientsTextView.append("\n"); //put extra line between header and ingredients
+		for(int i = 0 ; i < ingredients.length; i++) {
+				ingredientsTextView.append("\n" + ingredients[i]);
+			
+		}
+	}
+	
+	private void setGlassPicture() {
+		ImageView glassTextView = (ImageView) findViewById(R.id.drink_info_glass_type);
+		int imageID = getResources().getIdentifier("yourpackagename:drawable/ic_launcher", null, null);
+		glassTextView.setImageResource(imageID);
+	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
