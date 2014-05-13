939e87f7d97413ee6c1e0567d8818241176835f5
lueyja1 <lueyja1@gmail.com>
2014-05-12 17:17:47 -0700

    First draft of KNN

--- OneDrinkAway/src/com/onedrinkaway/model/machinelearning/KNearestNeighborModel.java
+++ OneDrinkAway/src/com/onedrinkaway/model/machinelearning/KNearestNeighborModel.java
@@ -1,6 +1,7 @@
 package com.onedrinkaway.model.machinelearning;
 
 import java.util.*;
+import java.lang.Math;
 import com.onedrinkaway.common.*;
 
 public class KNearestNeighborModel implements MLModel {
@@ -11,6 +12,28 @@ public class KNearestNeighborModel implements MLModel {
   }
   
   public double predictRating(Drink sample) {
-    return 0.0;
+    double rating = 0.0;
+    for(Drink d : trainingSet){
+      double absDiff = calculateAbsoluteDifference(d, sample);
+      double signDiff = calculateSignedDifference(d, sample);
+      rating += d.rating * absDiff / 5;
+    }
+    return rating / trainingSet.size();
+  }
+  
+  private double calculateAbsoluteDifference(Drink a, Drink b){
+   double diff = 0.0;
+   for(int i = 0; i < a.attributes.length; i++){
+      diff += Math.pow(a.attributes[i] - b.attributes[i], 2);
+   }
+   return Math.sqrt(diff); 
+  }
+  
+  private double calculateSignedDifference(Drink a, Drink b){
+   double diff = 0.0;
+   for(int i = 0; i < a.attributes.length; i++){
+     diff += a.attributes[i] - b.attributes[i];
+   }
+   return diff;
   }
 }
