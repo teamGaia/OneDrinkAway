aaa101560ee8b1f2b066e96bb8de38b96859fb84
John L Wilson <johnleewilson4@gmail.com>
2014-05-13 10:59:29 -0700

    implemented DrinkInfo class, added DrinkData class, which is a helper for DrinkDb

---
+++ OneDrinkAway/src/com/onedrinkaway/db/DrinkData.java
@@ -0,0 +1,22 @@
+package com.onedrinkaway.db;
+
+import java.util.ArrayList;
+
+import com.onedrinkaway.common.Drink;
+
+public class DrinkData {
+    
+    ArrayList<Drink> drinks;
+    
+    public DrinkData() {
+        
+    }
+    
+    /**
+     * Builds this DrinkData from file
+     */
+    private void buildFromFile() {
+        
+    }
+
+}
