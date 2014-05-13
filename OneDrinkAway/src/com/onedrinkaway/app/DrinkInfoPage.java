37e8def294f85fa848be442d8125a3eb3588f9e3
TJuve <TJuve92@gmail.com>
2014-05-12 17:36:07 -0700

    Created ResultsPage that displays temp results. Also did other stuff...

--- OneDrinkAway/src/com/onedrinkaway/app/DrinkInfoPage.java
+++ OneDrinkAway/src/com/onedrinkaway/app/DrinkInfoPage.java
@@ -150,6 +150,7 @@ public class DrinkInfoPage extends OneDrinkAwayActivity {
 
 	    return super.onOptionsItemSelected(item);
 	}
+	
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
