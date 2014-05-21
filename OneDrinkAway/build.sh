6e7ba3c675f90cc82da8f5a0949df285bb247ca9
lueyja1 <lueyja1@gmail.com>
2014-05-21 15:04:04 -0700

    Update build.sh
    added comments and a usage message

--- OneDrinkAway/build.sh
+++ OneDrinkAway/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+# Takes in two parameters:
+# $1 should be path to your android build tools folder (usually under the sdk)
+# $2 should be the path to your appcompat-v7 project
+
 set -e
+
+if [ ! -n "$1" ]
+then
+  echo "Usage: first argument should be path to your android build tools folder (usually under the sdk)"
+  echo "       second argument should be the path to your appcompat-v7 project"
+  exit 1
+fi
+
 export ANDROID_HOME=$1
 
 export PATH=$ANDROID_HOME/tools:$PATH
