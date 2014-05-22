b564f3f432e49c655ab0cfe2db6f6376fb339277
TJuve <TJuve92@gmail.com>
2014-05-22 15:31:51 -0700

    Started work on the new user rating experience. Created an with two tabs, each tab has their own fragment class

---
+++ OneDrinkAway/src/com/onedrinkaway/app/NewUserRatingActivity.java
@@ -0,0 +1,24 @@
+package com.onedrinkaway.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+
+import com.onedrinkaway.R;
+
+public class NewUserRatingActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_user_rating);
+		
+		FragmentTabHost mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Find and Rate"),
+            NewUserRateByName.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("Rate Common Drinks"),
+            NewUserRateCommonDrinks.class, null);
+	}
+}
