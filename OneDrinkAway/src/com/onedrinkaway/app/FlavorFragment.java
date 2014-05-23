0e90e4965811f8db02d6fae6c9fbe9580ec1aa8c
TJuve <TJuve92@gmail.com>
2014-05-22 20:19:56 -0700

    Advanced search displays all tabs correctly.

--- OneDrinkAway/src/com/onedrinkaway/app/FlavorFragment.java
+++ OneDrinkAway/src/com/onedrinkaway/app/FlavorFragment.java
@@ -1,16 +1,101 @@
 package com.onedrinkaway.app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import com.onedrinkaway.R;
+import com.onedrinkaway.app.SearchByFlavor.FlavorSeekBarListener;
+import com.onedrinkaway.model.Flavor;
 
 public class FlavorFragment extends Fragment {
+	private TableLayout flavorsScrollViewTable;
+	
+	private Button flavorSearchButton;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	   return inflater.inflate(R.layout.fragment_flavor, null);
+	    View view = inflater.inflate(R.layout.fragment_flavor, null);
+	   
+		setUpView(view);
+		displayFlavors();	
+		
+		return view;
+	}
+	
+	private void setUpView(View view) {		
+		flavorsScrollViewTable = (TableLayout) view.findViewById(R.id.flavors_scroll_view_table);
+		flavorSearchButton = (Button) view.findViewById(R.id.flavor_search_button);
+	}
+
+	private void displayFlavors() {
+		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			
+		for (int i = 0; i < Flavor.flavorsArr.length; i++) {
+			// Set the TextView in search_by_flavor_row
+			View flavorRow = inflater.inflate(R.layout.activity_search_by_flavor_row, null);
+			TextView flavorTextView = (TextView) flavorRow.findViewById(R.id.flavor_text_view);
+			flavorTextView.setText(Flavor.flavorsArr[i]);
+			View flavorRow2 = inflater.inflate(R.layout.activity_search_by_flavor_row2, null);
+			// Set the SeekBar in search_by_flavor_row2
+			SeekBar seekbar = (SeekBar) flavorRow2.findViewById(R.id.flavor_seek_bar);
+			seekbar.setOnSeekBarChangeListener(new FlavorSeekBarListener(Flavor.flavorsArr[i]));
+			View flavorRow3 = inflater.inflate(R.layout.activity_search_by_flavor_row3, null);
+			// Add each row to the view
+			flavorsScrollViewTable.addView(flavorRow, i * 3);
+			flavorsScrollViewTable.addView(flavorRow2, i * 3 + 1);
+			flavorsScrollViewTable.addView(flavorRow3, i * 3 + 2);
+		}
+	}
+	
+	/**
+	 * Class for a Seek Bar Listener that adjusts what flavors to search by depending
+	 * on the number the user adjusts the seek bar to
+	 */
+	public class FlavorSeekBarListener implements OnSeekBarChangeListener {
+		int progressChanged = 0;
+		String flavor;
+		
+		/**
+		 * Creates a new FlavorSeekBarListener
+		 * @param flavor: the flavor this seek bar is for
+		 */
+		FlavorSeekBarListener(String flavor) {
+			this.flavor = flavor;
+		}
+	
+		/**
+		 * Adjusts the flavor intensity to search by when the user changes
+		 * the progress on this flavor's seek bar
+		 */
+		@Override
+		public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser) {
+			progressChanged = progress;
+			Flavor newFlavor = new Flavor(flavor, progressChanged);
+//			query.add(newFlavor);
+//			Log.i("Query size", query.getFlavors().size() + "");
+		}
+		
+
+		@Override
+		public void onStartTrackingTouch(SeekBar arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
